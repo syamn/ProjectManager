bd2c6b58328f7efb9a0d1ea279f574d3485af128
syamn <admin@sakura-server.net>
2012-08-14 11:22:33 +0900

    Save/Load joined players list when plugin onDisable/onEnable

--- ThemeCreative/src/syam/ThemeCreative/Theme/ThemeFileManager.java
+++ ThemeCreative/src/syam/ThemeCreative/Theme/ThemeFileManager.java
@@ -2,12 +2,19 @@ package syam.ThemeCreative.Theme;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import syam.ThemeCreative.ThemeCreative;
+import syam.ThemeCreative.Enum.MemberType;
+import syam.ThemeCreative.Util.Actions;
 
 public class ThemeFileManager{
 	// Logger
@@ -29,10 +36,14 @@ public class ThemeFileManager{
 		for (Theme theme : plugin.themes.values()){
 			File file = new File(fileDir + theme.getName() + ".yml");
 
+			// マップデータをリストに変換
+			List<String> playerList = convertPlayerMap(theme.getPlayersMap());
+
 			// 保存するデータ
 			confFile.set("ThemeName", theme.getName());
 			confFile.set("Title", theme.getTitle());
 
+			confFile.set("Players", playerList);
 
 			try{
 				confFile.save(file);
@@ -74,7 +85,7 @@ public class ThemeFileManager{
 				theme.setFileName(file.getName());
 
 				// 各設定データを追加
-				// do stuff..
+				theme.setPlayersMap(convertPlayerMap(confFile.getStringList("Players")));
 
 				log.info(logPrefix+ "Loaded Theme: "+ file.getName()+" ("+name+")");
 			}
@@ -84,4 +95,56 @@ public class ThemeFileManager{
 		}
 	}
 
+	/* 各種データ変換 */
+	private List<String> convertPlayerMap(Map<String, MemberType> map){
+		List<String> ret = new ArrayList<String>();
+		ret.clear();
+
+		for (Map.Entry<String, MemberType> entry : map.entrySet()){
+			// memberType@playerName リスト追加
+			ret.add(entry.getValue().name() + "@" + entry.getKey());
+		}
+
+		return ret;
+	}
+
+	private Map<String, MemberType> convertPlayerMap(List<String> list){
+		Map<String, MemberType> ret = new HashMap<String, MemberType>();
+		ret.clear();
+
+		String[] data;
+
+		int line = 0;
+		for (String s : list){
+			line++;
+			// デリミタ分割
+			data = s.split("@");
+			if (data.length != 2){
+				log.warning(logPrefix+ "Skipping PlayerLine "+line+": incorrect format(@)");
+				continue;
+			}
+
+			// data[0] : タイプチェック
+			MemberType type = null;
+			for (MemberType mt : MemberType.values()){
+				if (mt.name().equalsIgnoreCase(data[0])){
+					type = mt;
+				}
+			}
+			if (type == null){
+				log.warning(logPrefix+ "Skipping PlayerLine "+line+": undefined MemberType");
+				continue;
+			}
+
+			// data[1] : プレイヤー名チェック
+			final Pattern pattern = Pattern.compile("^\\w{2,16}$");
+			if (!pattern.matcher(data[1]).matches()){
+				log.warning(logPrefix+ "Skipping PlayerLine "+line+": incorrect PlayerName");
+				continue;
+			}
+
+			ret.put(data[1], type);
+		}
+		return ret;
+	}
 }
