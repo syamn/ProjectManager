eef5fc698477a257fcb86f975219eee26c4dc72f
syamn <admin@sakura-server.net>
2012-08-14 17:49:05 +0900

    Add promotion/demotion command

--- ThemeCreative/src/syam/ThemeCreative/Command/MemberCommand.java
+++ ThemeCreative/src/syam/ThemeCreative/Command/MemberCommand.java
@@ -53,16 +53,21 @@ public class MemberCommand extends BaseCommand{
 
 		// アクションによって処理を分ける
 		switch (ac){
+			// メンバーリスト表示
+			case LIST:
+				return list(theme);
 			// メンバー追加
 			case ADD:
 				return add(theme);
 			// メンバー削除
 			case DEL:
 				return del(theme);
-			// メンバーリスト表示
-			case LIST:
-				return list(theme);
-
+			// マネージャにする
+			case PROMOTION:
+				return promotion(theme);
+			// マネージャを解除する
+			case DEMOTION:
+				return demotion(theme);
 
 			// 定義漏れ
 			default:
@@ -75,6 +80,28 @@ public class MemberCommand extends BaseCommand{
 	}
 
 	/* ***** ここから各アクション分岐 ************************ */
+
+	// LIST - メンバーリスト表示
+	private boolean list(Theme theme){
+		// 人数出力
+		if (theme.getPlayersMap().size() < 1){
+			Actions.message(null, player,"&6このプロジェクトに参加しているプレイヤーはいません");
+			return true;
+		}else{
+			Actions.message(null, player,"&6このプロジェクトには &c"+theme.getPlayersMap().size()+"人 &6のプレイヤーが参加しています");
+		}
+
+		// マネージャ出力
+		if (theme.getPlayersByType(MemberType.MANAGER).size() > 1){
+			Actions.message(null, player,"マネージャ: " + Util.join(theme.getPlayersByType(MemberType.MANAGER), ", "));
+		}
+		// 一般メンバー出力
+		if (theme.getPlayersByType(MemberType.MEMBER).size() > 1){
+			Actions.message(null, player,"メンバー: " + Util.join(theme.getPlayersByType(MemberType.MEMBER), ", "));
+		}
+
+		return true;
+	}
 
 	// ADD - メンバー追加
 	private boolean add(Theme theme){
@@ -130,29 +157,68 @@ public class MemberCommand extends BaseCommand{
 		return true;
 	}
 
-	// LIST - メンバーリスト表示
-	private boolean list(Theme theme){
-		// 人数出力
-		if (theme.getPlayersMap().size() < 1){
-			Actions.message(null, player,"&6このプロジェクトに参加しているプレイヤーはいません");
+	// PROMOTION - マネージャにする
+	private boolean promotion(Theme theme){
+		// プレイヤー名チェック
+		if(args.size() == 1){
+			Actions.message(sender, null, "&cアクションを行う対象のプレイヤー名を入力してください");
 			return true;
-		}else{
-			Actions.message(null, player,"&6このプロジェクトには &c"+theme.getPlayersMap().size()+"人 &6のプレイヤーが参加しています");
+		}
+		final Pattern pattern = Pattern.compile("^\\w{2,16}$");
+		if (!pattern.matcher(args.get(1)).matches()){
+			Actions.message(sender, null, "&cプレイヤー名が不正です！");
+			return true;
 		}
 
-		// マネージャ出力
-		if (theme.getPlayersByType(MemberType.MANAGER).size() > 1){
-			Actions.message(null, player,"マネージャ: " + Util.join(theme.getPlayersByType(MemberType.MANAGER), ", "));
+		String name = args.get(1);
+
+		if (theme.isManager(name)){
+			Actions.message(sender, null, "&cプレイヤー'"+name+"'は既にマネージャーになっています");
+			return true;
 		}
-		// 一般メンバー出力
-		if (theme.getPlayersByType(MemberType.MEMBER).size() > 1){
-			Actions.message(null, player,"メンバー: " + Util.join(theme.getPlayersByType(MemberType.MEMBER), ", "));
+		if (!theme.isJoined(name)){
+			Actions.message(sender, null, "&cプレイヤー'"+name+"'はプロジェクトに参加していません");
+			return true;
 		}
+
+		theme.addManager(name);
+		Actions.message(null, player, "&aプレイヤー '" + name + "' をプロジェクトマネージャに昇格させました！");
 
 		return true;
 	}
 
-	/* ***** ここまで***************************************** */
+	// DEMOTION - マネージャを解除する
+	private boolean demotion(Theme theme){
+		// プレイヤー名チェック
+		if(args.size() == 1){
+			Actions.message(sender, null, "&cアクションを行う対象のプレイヤー名を入力してください");
+			return true;
+		}
+		final Pattern pattern = Pattern.compile("^\\w{2,16}$");
+		if (!pattern.matcher(args.get(1)).matches()){
+			Actions.message(sender, null, "&cプレイヤー名が不正です！");
+			return true;
+		}
+
+		String name = args.get(1);
+
+		if (theme.isJoined(name) && !theme.isManager(name)){
+			Actions.message(sender, null, "&cプレイヤー'"+name+"'は既にマネージャーになっています");
+			return true;
+		}
+		if (!theme.isJoined(name)){
+			Actions.message(sender, null, "&cプレイヤー'"+name+"'はプロジェクトに参加していません");
+			return true;
+		}
+
+		theme.addMember(name);
+		Actions.message(null, player, "&aプレイヤー '" + name + "' をプロジェクトマネージャから降格させました！");
+
+		return true;
+	}
+
+
+	/* ***** ここまで ***************************************** */
 
 	/**
 	 * 実行可能なアクションをsenderに送信する
@@ -171,9 +237,11 @@ public class MemberCommand extends BaseCommand{
 	 * @author syam
 	 */
 	enum Action{
-		ADD,	// メンバー追加
-		DEL,	// メンバー削除
-		LIST,	// メンバーリスト表示
+		LIST,		// メンバーリスト表示
+		ADD,		// メンバー追加
+		DEL,		// メンバー削除
+		PROMOTION,	// マネージャにする
+		DEMOTION,	// マネージャを解除する
 		;
 	}
 
