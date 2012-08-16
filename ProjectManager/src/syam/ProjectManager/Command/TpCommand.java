2a07c11139787c13f9c488b631ade08241046594
syamn <admin@sakura-server.net>
2012-08-16 23:28:59 +0900

    Fixes not working tp command

--- ProjectManager/src/syam/ProjectManager/Command/TpCommand.java
+++ ProjectManager/src/syam/ProjectManager/Command/TpCommand.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
+
 import syam.ProjectManager.Project.Project;
 import syam.ProjectManager.Util.Actions;
 
@@ -18,31 +22,50 @@ public class TpCommand extends BaseCommand{
 	@Override
 	public boolean execute() {
 		Project project = null;
-		Boolean joinedProject = false;
 		if (args.size() == 0){
 			List<Project> joined = plugin.getJoinedProject(player.getName());
 			if (joined.size() == 1){
-				project = joined.get(0);
+				project = joined.get(0); // Set project
 				Actions.message(null, player, "&a参加中のプロジェクト'"+project.getID()+"'にテレポートします！");
 			}
 			else if(joined.size() == 0){
-				Actions.message(null, player, "&c参加中のプロジェクトがありません！ /project tp <プロジェクト名>");
-				return true;
+				Actions.message(null, player, "&c参加中のプロジェクトがありません！ /project tp <プロジェクトID>");
 			}
 			else{
-				Actions.message(null, player, "&c参加中のプロジェクトが複数あります！ /project tp <プロジェクト名>");
-				return true;
+				Actions.message(null, player, "&c参加中のプロジェクトが複数あります！ /project tp <プロジェクトID>");
 			}
-		}else{
+		}
+		else{
 			Project t = plugin.getProject(args.get(0));
 			if (t == null){
 				Actions.message(null, player, "&cプロジェクトID'"+args.get(0)+"'が見つかりません！");
-				return true;
 			}else{
-				project = t;
+				project = t; // Set project
+				Actions.message(null, player, "&aプロジェクトID'"+project.getID()+"'にテレポートします！");
 			}
 		}
 
+		// null return
+		if (project == null)
+			return true;
+
+		Location loc = project.getWarpLocation();
+		if (loc == null){
+			Actions.message(null, player, "&cこのプロジェクトはワープ地点が未設定です！");
+			return true;
+		}
+
+		// Teleport
+		player.teleport(loc, TeleportCause.PLUGIN);
+
+		// Check project gamemode
+		if (project.getCreative()){
+			player.setGameMode(GameMode.CREATIVE);
+			Actions.message(null, player, "&aこのプロジェクトはクリエイティブモードが有効になっています！");
+		}else{
+			player.setGameMode(GameMode.SURVIVAL);
+			Actions.message(null, player, "&aプロジェクトの拠点にテレポートしました！");
+		}
 
 		return true;
 	}
